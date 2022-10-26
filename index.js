b665867d6cfda5e2d98e1da4ec5a847337545881
Viktor Bogutskii <info@siteograf.com>
2022-10-26 01:30:52 -0400

    update. connection db

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ routes(app);
 (async () => {
   try {
     await sequelize.authenticate();
-    // await sequelize.sync();
+    await sequelize.sync();
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
     console.log(e);
