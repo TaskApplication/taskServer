afedbbc85f6ba613a831c68d766159d3e9252ef3
Viktor Bogutskii <info@siteograf.com>
2022-10-28 22:27:56 -0400

    CRUD operations to DB

--- v1/task/create.js
+++ v1/task/create.js
@@ -3,7 +3,7 @@ const Task = require('./Model');
 async function createTask(req, res) {
   const { title, description } = req.body;
 
-  const task = await Task.create({
+  await Task.create({
     title,
     description,
     birthday: new Date(1980, 6, 20),
