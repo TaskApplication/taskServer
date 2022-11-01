4ab51bcf2aa93e08004cf01ea08c779ffc5ed05a
Viktor Bogutskii <info@siteograf.com>
2022-10-31 21:33:25 -0400

    update cors

--- cors.js
+++ cors.js
@@ -1,14 +1,19 @@
 const cors = require('cors');
 
 const corsModule = (app) => {
-  app.use(
-    cors({
-      origin: process.env.CLIENT_HOST,
-      credentials: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-    }),
-  );
+  app.use((req, res, next) => {
+    // eslint-disable-line consistent-return
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    // if (req.method === 'OPTIONS') {
+    //   return res.status(200).json({});
+    // }
+    next();
+  });
 };
 
 module.exports = corsModule;
