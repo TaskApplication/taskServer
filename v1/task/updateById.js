bcbf4886fdc73d5afa9f9a3c5392160cc5791fcf
Viktor Bogutskii <info@siteograf.com>
2022-10-31 20:46:32 -0400

    update controller

--- v1/task/updateById.js
+++ v1/task/updateById.js
@@ -1,5 +1,24 @@
+const Task = require('./Model');
+
 function updateByIdTask(req, res) {
-  res.send('updateByIdTask 123');
+  // get params from request
+  const { id } = req.params;
+  const { title, description } = req.body;
+
+  // update task in database by id
+  Task.update({
+    title,
+    description
+  }, {
+    where: {
+      id
+    }
+  }).then((task) => {
+    res.send('Task updated successfully');
+  }).catch((err) => {
+    res.send('Task update Error');
+  });
+
 }
 
 module.exports = updateByIdTask;
